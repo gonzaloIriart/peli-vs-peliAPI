d722bada0567d73db12f83d08ef88924364cdec3
GonzaloIriart <gonzaloiriart@gmail.com>
2019-09-29 12:11:11 -0300

    docker

--- servidor/lib/conexiondb.js
+++ servidor/lib/conexiondb.js
@@ -2,11 +2,11 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection(
     {
-        host     : 'localhost',
-        user     : 'root',
-        password : 'root',
-        database: 'competencias',
-        port: 3306,
+        host     : process.env.DB_HOST,
+        user     : process.env.DB_USER,
+        password : process.env.DB_PASS,
+        database : process.env.DB_DATABASE,
+        port     : process.env.DB_PORT
       }
 );
 module.exports = connection;
